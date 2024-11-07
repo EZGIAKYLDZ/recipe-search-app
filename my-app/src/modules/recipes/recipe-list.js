eb33ec139b52a63d77050a042c11a74973fb5065
Ezgi Akyildiz <ezgii.akyildiz@gmail.com>
2024-11-07 17:55:32 +0300

    recipe-lesson36

--- my-app/src/modules/recipes/recipe-list.js
+++ my-app/src/modules/recipes/recipe-list.js
@@ -1,19 +1,18 @@
 import '../../App.css';
 
-export const RecipeList = ({ recipes, handleShowRecipe }) => {
+export const RecipeList = ({ recipes, handleShowRecipe,selectedRecipe }) => {
     return (
         recipes.length > 0 ? (
             <ul className='card-container'>
                 {recipes.map((recipe) => (
                     <div className='card-product'>
-                        <img src={recipe.strMealThumb} alt={recipe.strMeal}></img>
-                        <li key={recipe.idMeal} onClick={() => handleShowRecipe(recipe.idMeal)}>
+                        <img src={recipe.strMealThumb} alt={recipe.strMeal} onClick={() => handleShowRecipe(recipe.idMeal)}></img>
+                        <li className='recipe-name' key={recipe.idMeal} onClick={() => handleShowRecipe(recipe.idMeal)}>
                             {recipe.strMeal}
                         </li>
-                        {/* <p>{selectedRecipe}</p> 
                         {selectedRecipe.idMeal === recipe.idMeal ? 
                         <p>{selectedRecipe.strInstructions}</p> : <></>
-                        } */}
+                        }
                     </div>
                 ))}
             </ul>
